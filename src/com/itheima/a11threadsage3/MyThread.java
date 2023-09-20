4b1481d438787f02efbf23ca480783cb869207df
lcq <ChuncyLi@163.com>
2023-09-20 17:59:55 +0800

    1. Java多线程Lock实现

---
+++ src/com/itheima/a11threadsage3/MyThread.java
@@ -0,0 +1,30 @@
+package com.itheima.a11threadsage3;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyThread extends Thread {
+
+    static int ticket = 0;
+
+    Lock lock = new ReentrantLock();
+
+    @Override
+    public void run(){
+        while (true) {
+            lock.lock();
+                if(ticket < 100) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(Thread.currentThread().getName() + "正在卖第" + ticket + "张票");
+                    ticket++;
+                } else {
+                    break;
+                }
+            lock.unlock();
+        }
+    }
+}
---
+++ src/com/itheima/a11threadsage3/ThreadDemo.java
@@ -0,0 +1,21 @@
+package com.itheima.a11threadsage3;
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+        // 创建线程对象
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        MyThread t3 = new MyThread();
+
+        t1.setName("窗口1");
+        t2.setName("窗口2");
+        t3.setName("窗口3");
+
+        // 启动线程
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
