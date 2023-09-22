005cff58c028a928d4a3898edb8521026941b6c6
lcq <ChuncyLi@163.com>
2023-09-22 17:13:55 +0800

    完成生产者代码

--- src/com/itheima/a13waitandnotify/Cook.java
+++ src/com/itheima/a13waitandnotify/Cook.java
@@ -14,7 +14,24 @@ public class Cook extends Thread {
             synchronized (Desk.lock) {
                 // 结束条件 判断还能吃多少面条
                 if (Desk.count == 0) {
-
+                    break;
+                } else {
+                    // 判断桌子上是否有食物
+                    if (Desk.foodFlag == 1) {
+                        // 如果有，就等待
+                        try {
+                            Desk.lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        // 如果没有，就制作食物
+                        System.out.println("厨师正在制作食物，还能制作" + Desk.count + "份");
+                        // 修改桌子上的食物状态
+                        Desk.foodFlag = 1;
+                        // 叫醒等待的消费者开吃
+                        Desk.lock.notifyAll();
+                    }
                 }
             }
         }
